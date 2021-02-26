d761666d6dfa3236d6259fc79a06d80343cd1864
Zak <zakaria@smoc.ai>
2021-02-26 15:26:58 +0100

    Initial commit

---
+++ src/test/java/com/qascript/FirstTest.java
@@ -0,0 +1,27 @@
+package com.qascript;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstTest {
+
+
+    @Test
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+    }
+
+
+}
+
+
